18d8394285cf3a68881a24a8bd584a60c989896f
pham1123 <trien13pham@gmail.com>
2021-01-17 13:20:55 -0500

    Some cosmetic adjustments

--- euler0014.js
+++ euler0014.js
@@ -6,26 +6,26 @@ var bestNum;
 
 var tempLength = 0; // used by collatz() in order to have a counter that survives after each recursion
 
-function collatz(n) {
+function getCollatzLength(n) {
     let num = n;
     if (num == 1) { // end case n = 1: add 1 to length count and return it 
         tempLength++;
         return tempLength;
     } else if (num % 2 == 0) { // n is even
         tempLength++;
-        return collatz(num / 2);
+        return getCollatzLength(num / 2);
     } else if (n % 2 != 0) { // n is odd 
         tempLength++;
-        return collatz(3 * num + 1);
+        return getCollatzLength(3 * num + 1);
     }
 }
 
 for (let i = 1; i < 1000000; i++) {
     tempLength = 0;
-    let newLength = collatz(i);
-    if (bestLength < newLength) {
+    let currentLength = getCollatzLength(i);
+    if (bestLength < currentLength) {
         bestNum = i;
-        bestLength = newLength;
+        bestLength = currentLength;
     }
 }
 
