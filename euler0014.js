31641849cb5a48bc2864d6a3bf72b0f5d21225e2
pham1123 <23087717+pham1123@users.noreply.github.com>
2021-02-14 19:41:57 -0500

    variable cleanup

--- euler0014.js
+++ euler0014.js
@@ -7,16 +7,16 @@ var bestNum;
 var tempLength = 0; // used by collatz() in order to have a counter that survives after each recursion
 
 function getCollatzLength(n) {
-    let num = n;
-    if (num == 1) { // end case n = 1: add 1 to length count and return it 
+    //let num = n;
+    if (n == 1) { // end case n = 1: add 1 to length count and return it 
         tempLength++;
         return tempLength;
-    } else if (num % 2 == 0) { // n is even
+    } else if (n % 2 == 0) { // n is even
         tempLength++;
-        return getCollatzLength(num / 2);
+        return getCollatzLength(n / 2);
     } else if (n % 2 != 0) { // n is odd 
         tempLength++;
-        return getCollatzLength(3 * num + 1);
+        return getCollatzLength(3 * n + 1);
     }
 }
 
