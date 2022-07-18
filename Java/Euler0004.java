e8f90573f9b5c54fa895844e0e5efd661b1c5809
havesomepham <23087717+havesomepham@users.noreply.github.com>
2022-07-17 20:55:37 -0400

    Recursion for Euler #4

--- Java/Euler0004.java
+++ Java/Euler0004.java
@@ -6,35 +6,17 @@
  */
 
 public class Euler0004 {
-    
-    // check if n is a palindrome
-    public static boolean palindromeCheck(int n) { 
-        String str = Integer.toString(n);
-        String head, tail;
-        
-        // cut the number String into two parts to be compared
-        if (str.length() % 2 == 0) { // if the palindrome has an even number of digits
-            head = str.substring(0, str.length()/2);
-            tail = str.substring(str.length()/2);
-        } else {
-            head = str.substring(0, (str.length() - 1)/2);
-            tail = str.substring((str.length() + 1)/2 );
-        }
-        if (head.equals(stringReversal(tail))) {
+
+    // a recursive solution
+    public static boolean isPalindrome(String str) {
+        if (str.length() < 2) { // if the final string is zero or one char long, we've found a palindrome
             return true;
+        }
+        if (str.charAt(0) == str.charAt(str.length() - 1)) { // if the first and last chars match...
+            return isPalindrome(str.substring(1, str.length() - 1)); // ...cut them out and call isPalindrome on the shorter string
         } else {
             return false;
         }
-    }
-
-    // return reversed string
-    public static String stringReversal(String str) {
-        String reversed = "";
-        for (int i = 0; i < str.length(); i++) {
-            String ch = Character.toString(str.charAt(str.length() - i - 1));
-            reversed = reversed.concat(ch);
-        }
-        return reversed;
     }
 
     public static void main(String[] args) {
@@ -42,8 +24,8 @@ public class Euler0004 {
 
         for (int i = 100; i < 1000; i++) {
             for (int j = 100; j < 1000; j++) {
-                if (palindromeCheck(i*j) && i*j > largestPalindrome) {
-                    largestPalindrome = i*j;
+                if (isPalindrome(Integer.toString(i * j)) && i * j > largestPalindrome) {
+                    largestPalindrome = i * j;
                 }
             }
         }
